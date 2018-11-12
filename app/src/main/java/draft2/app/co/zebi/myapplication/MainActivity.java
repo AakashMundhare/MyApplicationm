c0e7c9e3de98bdd7ecec9f668f241f0c507a222f
AakashMundhare <aakash.mundhare@zebi.co>
2018-11-12 16:18:27 +0530

    Initial commit

---
+++ app/src/main/java/draft2/app/co/zebi/myapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package draft2.app.co.zebi.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
